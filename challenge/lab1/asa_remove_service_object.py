68bae7caf63b69d0ea4363ceea34f583b5b54aa5
Tom Lijnse <tom.lijnse@layerzero.nl>
2017-08-24 11:14:00 +0200

    Added challenge lab 1

---
+++ challenge/lab1/asa_remove_service_object.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""NPDESI challenge lab 1 task 4: Construct Python Script Consuming ASA API"""
+
+import requests
+import json
+requests.packages.urllib3.disable_warnings()
+
+def main():
+    auth = requests.auth.HTTPBasicAuth('cisco', 'cisco')
+    url = 'https://asa/api/objects/networkservicegroups/ssh-alt'
+    response = requests.delete(url, verify=False, auth=auth)
+    print 'Status Code: ' + str(response.status_code)
+    if response.text:
+        parse = json.loads(response.text)
+        print json.dumps(parse, indent=4)
+
+
+if __name__ == "__main__":
+    main()
+
